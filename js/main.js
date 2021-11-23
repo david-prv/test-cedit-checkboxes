07cc3c92e86807c11d76b7fde517072f50c33976
david-prv <66866223+david-prv@users.noreply.github.com>
2021-11-23 19:10:36 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,45 @@
+function handleMouseLeave(event) {
+    let text = event.path[0].innerHTML.replace(`<input type="checkbox" id="dewey" name="drone" value="dewey">`, "");
+    text = text.replace('<br>', '');
+    let id = event.path[0].id;
+    if(text.length < 1) {
+        console.log("Element empty: ", id);
+        let el = document.getElementById(id);
+        el.parentNode.removeChild(el);
+    }
+}
+
+function blockClick(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    console.log("AWD");
+}
+
+function toggleCEdit() {
+    let el = document.getElementById('checkbox-ceditable');
+    if(el.getAttribute('contenteditable') === "true") {
+        el.setAttribute('contenteditable', 'false');
+        document.getElementById('lbl').innerHTML = "Toggle contentEditable (OFF)";
+        Array.prototype.forEach.call(document.getElementsByTagName('label'), function (item, i, arr) {
+            item.removeEventListener('click', blockClick);
+        });
+    } else {
+        el.setAttribute('contenteditable', 'true');
+        document.getElementById('lbl').innerHTML = "Toggle contentEditable (ON)";
+        Array.prototype.forEach.call(document.getElementsByTagName('label'), function (item, i, arr) {
+            item.addEventListener('click', blockClick);
+        });
+    }
+}
+
 function firedup() {
     let el = document.getElementById('checkbox-ceditable');
-    console.log(el);
+
+    Array.prototype.forEach.call(document.getElementsByTagName('label'), function (item, i, arr) {
+        item.addEventListener('mouseleave', handleMouseLeave);
+        item.addEventListener('click', blockClick);
+    });
+
     el.addEventListener('keydown', function(event) {
         if(event.key === "Enter") {
             event.preventDefault();
@@ -9,16 +48,16 @@ function firedup() {
             let li = document.createElement('li');
             let l = document.createElement('label');
             let x = document.createElement('input');
-            li.id = "new-" + Date.now();
+            l.id = "new-" + Date.now();
             x.type = "checkbox";
-            x.id = "dewey-" + Date.now();
+            x.id = "dewey";
             x.name = "drone"
             x.value = "dewey";
             ev.appendChild(li);
             li.appendChild(l);
             l.appendChild(x);
-            l.innerHTML += " Test";
-            document.getElementById(li.id).focus();
+            l.innerHTML += "Test";
+            l.addEventListener('mouseleave', handleMouseLeave);
         }
     });
 }
